da58670a55b92569d90580f6043d2fb0862cf514
pangshishan <pangshishan@mail.51vv.com>
2021-02-13 23:06:40 +0800

    swift语法 - 20210213

--- TestSwift/TestSwift/main.swift
+++ TestSwift/TestSwift/main.swift
@@ -262,41 +262,428 @@ import Foundation
  
  在getFn返回时，plus才捕获num，如果返回的不是plus，则不会捕获，也不会申请堆空间内存。
  
- 如果num是全局变量，不会捕获num了，不会申请堆空间内存了。申请堆空间是为了避免num是局部变量时被释放掉
+ 如果num是全局变量，不会捕获num了，不会申请堆空间内存了。申请堆空间是为了避免num是局部变量时被释放掉。
+ 
+ 如果 将num换成Class，并且Class创建局部变量实例，会是怎样呢。
  
  */
 
 
 //
-//func sum(_ v1: Int, _ v2: Int) -> Int {
-//    v1 + v2
+//typealias Fn = (Int) -> (Int, Int)
+//
+//func getFns() -> (Fn, Fn) {
+//    var num1 = 0
+//    var num2 = 0
+//    func plus(_ i: Int) -> (Int, Int) {
+//        num1 += i
+//        num2 += i << 1
+//        return (num1, num2)
+//    }
+//    func minus(_ i: Int) -> (Int, Int) {
+//        num1 -= i
+//        num2 -= i << 1
+//        return (num1, num2)
+//    }
+//    return (plus, minus)
 //}
-//var fn = sum
-//print(MemoryLayout.stride(ofValue: fn))
+//let (p, m) = getFns()
+//print(p(5))
+//print(m(4))
+//print(p(3))
+//print(m(2))
+
+/*
+ // 以上代码，相当于生成了一个闭包对象，num1和num2是两个成员变量，plus和minus是闭包的两个函数。
+ 
+ num1 和 num2 分别分配了两块内存，都是 8 8 8 ，第三个字节分别存着num1和num2
+ */
+
+//var functions: [() -> Int] = []
+//for i in 1...3 {
+//    functions.append({ i })
+//}
+//for f in functions {
+//    print(f())
+//}
 
 
-
+// MARK: 隐藏闭包
+//func getNumber() -> Int {
+//    let a = 10
+//    let b = 20
+//    print("getNumber")
+//    return a + b
+//}
+//func getFirstPositive(_ v1: Int, _ v2: Int) -> Int {
+//    return v1 > 0 ? v1 : v2;
+//}
+//getFirstPositive(10, 20)
+//getFirstPositive(-10, 20)
 //
-//var s1 = "111"
-//var s2 = s1
-//s2.append("_222")
-//print(s1)
-//print(s2)
+//getFirstPositive(10, getNumber())
 
+//func getFirstPositive(_ v1: Int, _ v2: () -> Int) -> Int {
+//    return v1 > 0 ? v1 : v2();
+//}
+//getFirstPositive(-10) { () -> Int in
+//    let a = 10
+//    let b = 20
+//    print("block")
+//    return a + b
+//}
+// 也可以简化闭包，或写成简化的隐藏闭包
+
+//func getFirstPositive(_ v1: Int, _ v2: @autoclosure () -> Int) -> Int {
+//    return v1 > 0 ? v1 : v2();
+//}
+//getFirstPositive(-10, 20)
+/*
+ @autoclosure 会自动将20 封装成闭包表达式{20}
+ @autoclosure 只支持 () -> T 这种格式的参数
+ @autoclosure 并非只支持最后一个参数
+ 
+ ?? 这个运算符就是使用了 @autoclosure
+ 有@autoclosure、无@autoclosure 构成了函数重载
+ 为了避免与期望冲突，使用了@autoclosure的地方最好明确注释清楚：这个值会被推迟执行
+ */
+//getFirstPositive(10, getNumber()) // 推迟执行
+
+//var a: Int? = 10
+//var b: Int? = nil
+//print(b ?? a)
+
+// MARK:- 属性
+/*
+ 属性分为实例属性和类型属性
+ 
+ 实例属性，只能通过实例去访问，每个实例都有一份；
+ 
+ Swift中跟实例相关的属性可以分为两大类
+ 
+ - 存储属性
+    类似于成员变量
+    存储在实例的内存中
+    结构体和类可以定义存储属性
+    枚举 不可以 定义存储属性
+ 
+ - 计算属性
+    本质就是方法（函数）
+    不占用实例的内存
+    结构体、类、枚举 都可以定义存储属性
+    计算属性只能用var，不能用let，原因是let修饰的是一个常量，但是计算属性实际上是方法。
+    如果只实现了get，就是只读的计算属性。不允许只有set
+ 
+ 关于存储属性，Swift有个明确的规定：
+ 在创建类或结构体实例时，必须为所有存储属性设置一个初始值。
+ 可以在初始化器里为存储属性设置一个初始值。也可以在定义属性时给个默认值
+ */
+
+//struct Circle {
+//    // 存储属性
+//    var radius: Double
+//    // 计算属性
+//    var diameter: Double {
+//        set {
+//            radius = newValue / 2
+//        }
+//        get {
+//            radius * 2
+//        }
+//    }
+//    // 只读计算属性
+////    var diameter: Double {
+////        get {
+////            radius * 2
+////        }
+////    }
 //
-//var dict1 = ["max": 10, "min": 0]
-//var dict2 = dict1
-//dict1["other"] = 7
-//print(dict1)
-//print(dict2)
+////    var diameter: Double { radius * 2 }
+//}
 
-//
-//var arr1 = [1, 2, 3]
-//var arr2 = arr1
-//arr2.append(4)
-//print(arr1)
-//print(arr2)
+//enum Season: Int {
+//    case spring = 4, summer, autumn, winter
+//    
+////    var rawValue: Int {
+////        switch self {
+////        case .spring:
+////            return 4
+////        case .summer:
+////            return 5
+////        case .autumn:
+////            return 6
+////        case .winter:
+////            return 7
+////        }
+////    }
+//}
+//let s = Season.spring
+//print(s.rawValue) // 实际上rawValue就是Swift生成的只读计算属性
 
+
+/*
+ // 延迟存储属性（懒加载）
+ 使用 lazy 可以定义一个延迟存储属性，在第一次用到属性的时候才会初始化。
+ lazy属性必须是var，不能是let
+ Swift规定，let的属性，在实例初始化完成之前必须有值
+ 
+ 多线程同时调用lazy的属性，可能会创建多个，不是线程安全的。
+ 
+ */
+//class Car {
+//    init() {
+//        print("Car init")
+//    }
+//    func run() {
+//        print("run ........")
+//    }
+//}
+//class Person {
+////    lazy var car = Car()
+//    lazy var car: Car = {
+//        print("111")
+//        print("111")
+//        print("111")
+//        print("111")
+//        return Car()
+//    }()
+//    init() {
+//        print("Person init")
+//    }
+//    func goOut() {
+//        car.run()
+//    }
+//}
+//let p = Person()
+//print(p)
+//p.goOut()
+
+// 当结构体定义延迟存储属性时，只有var才能访问延迟属性
+//struct Point {
+//    var x = 0
+//    var y = 0
+//    lazy var z = 0
+//}
+//let p = Point() // 将let给成var
+//print(p.z)
+
+/*
+ 属性观察器 Property Observer
+ 可以为非lazy的var存储属性设置属性观察器。
+ 在属性观察器中设置属性，不会触发willSet和didSet
+ */
+//struct Circle {
+//    // 存储属性
+//    var radius: Double {
+//        willSet {
+//            print("willSet", newValue)
+//        }
+//        didSet {
+//            print("didSet", oldValue, radius)
+//        }
+//    }
+//    init() {
+//        self.radius = 1.0
+//        print("Circle init")
+//    }
+//}
+
+// 属性观察器、计算属性的功能，同样可以应用在全局变量、局部变量身上
+
+/*
+ // 计算属性也是可以当成inout参数传到函数中的，而且也会成功调用计算属性的setter方法
+ 原理是：
+ 先调用计算属性的getter，将拿到的值存到一个临时变量里；
+ 然后将生成的临时变量地址值传给inout为参数的方法，并修改临时变量的值
+ 然后，将临时变量当成参数调用setter
+ 
+ // 如果将 带属性观察器的存储属性 用inout修饰传给函数，当函数内部修改属性值得时候，是会触发属性观察器的。
+ 也就是说，也会调用存储属性的setter。调用过程同上。
+ 
+ 如果不带属性观察器的存储属性，直接就是地址传递。
+ 
+ */
+
+/*
+ 实例属性，只能通过实例去访问，每个实例都有一份；
+ 
+ 类型属性，只能通过类型去访问，程序运行过程中，只有一份；关键字：static，如果是Class关键字是class或static。
+ 类型属性可以不给初始值。
+ 类型存储属性，默认就是lazy。
+ 是线程安全的。
+ 
+ 枚举也可以定义类型存储属性。
+ 
+ */
+
+// 单例
+//class FileManager {
+//    public static let shared: FileManager = FileManager()
+//    private init() { }
+//}
+
+
+// MARK:- 方法（Method）
+/*
+ 
+ - 枚举、结构体、类都可以定义实例方法、类型方法
+ 
+ 实例方法（Instance Method）：通过实例对象调用
+ 
+ 类型方法（Type Method）：通过类型调用，用static或者class关键字定义
+ 
+ */
+
+//class Car {
+//   static var cout = 0
+//   init() {
+//       Car.cout += 1
+//   }
+//   static func getCount() -> Int { cout }
+//}
+//let c0 = Car()
+//let c1 = Car()
+//let c2 = Car()
+//print(Car.getCount()) // 3
+/*
+ self
+ 在实例方法中代表实例对象
+ 在类型方法中代表类型
+ 
+ 在类型方法static func getCount中
+ cout等价于self.cout、Car.self.cout、Car.cout
+ */
+
+// MARK: mutating关键字
+
+/*
+ mutating
+ 
+ 结构体和枚举是值类型，默认情况下，值类型的属性不能被自身的实例方法修改
+ 在func关键字前加mutating可以允许这种修改行为
+ 
+ */
+//struct Point {
+//    var x = 0.0, y = 0.0
+//    mutating func moveBy(deltaX: Double, deltaY: Double) {
+//        x += deltaX
+//        y += deltaY
+//        // self = Point(x: x + deltaX, y: y + deltaY)
+//    }
+//}
+//enum StateSwitch {
+//    case low, middle, high
+//    mutating func next() {
+//        switch self {
+//        case .low:
+//            self = .middle
+//        case .middle:
+//            self = .high
+//        case .high:
+//            self = .low
+//        }
+//    }
+//}
+
+// MARK: @discardableResult 关键字
+
+/*
+ 在func前面加个@discardableResult，可以消除：函数调用后返回值未被使用的警告⚠
+ */
+//struct Point {
+//    var x = 0.0, y = 0.0
+//    @discardableResult mutating func moveX(deltaX: Double) -> Double {
+//        x += deltaX
+//        return x
+//    }
+//}
+//var p = Point()
+//p.moveX(deltaX: 10)
+//@discardableResult func get() -> Int {
+//    return 10
+//}
+//get()
+
+// MARK: 下标 subscripe
+
+/*
+ 
+ 使用subscript可以给任意类型(枚举、结构体、类)增加下标功能，有些地方也翻译为:下标脚本 psubscript的语法类似于实例方法、计算属性，本质就是方法(函数)
+   
+ subscript中定义的返回值类型决定了
+ get方法的返回值类型
+ set方法中newValue的类型
+ subscript可以接受多个参数，并且类型任意
+ 
+ */
+
+//class Point {
+//    var x = 0.0, y = 0.0
+//    subscript(index: Int) -> Double {
+//        set {
+//            if index == 0 {
+//                x = newValue
+//            } else if index == 1 {
+//                y = newValue
+//            }
+//        }
+//        get {
+//            if index == 0 {
+//                return x
+//            } else if index == 1 {
+//                return y
+//            }
+//            return 0
+//        }
+//    }
+//}
+//var p = Point()
+//p[0] = 11.1
+//p[1] = 22.2
+//print(p.x) // 11.1
+//print(p.y) // 22.2
+//print(p[0]) // 11.1
+//print(p[1]) // 22.2
+
+/*
+ - subscript可以没有set方法，但必须要有get方法。
+ - 如果只有get方法，可以省略get。
+ - 可以设置参数标签。
+ - 下标可以是类型方法。
+ */
+
+// 结构体、类作为返回值对比
+//class Point {
+//    var x = 0, y = 0
+//}
+//class PointManager {
+//    var point = Point()
+//    subscript(index: Int) -> Point {
+//        get { point }
+//    }
+//}
+//struct Point {
+//    var x = 0, y = 0
+//}
+//class PointManager {
+//    var point = Point()
+//    subscript(index: Int) -> Point {
+//        set { point = newValue }
+//        get { point } }
+//}
+//var pm = PointManager()
+//pm[0].x = 11
+//pm[0].y = 22
+//// Point(x: 11, y: 22)
+//print(pm[0])
+//// Point(x: 11, y: 22)
+//print(pm.point)
+
+// MARK:- 继承
+// 查看 语法.playground
+
+  
+ 
 
 
 
---
+++ 语法.playground/Pages/10-继承.xcplaygroundpage/Contents.swift
@@ -0,0 +1,339 @@
+//: [Previous](@previous)
+
+import Foundation
+
+
+/*
+ 值类型(枚举、结构体)不支持继承，只有类支持继承
+ 没有父类的类，称为:基类
+ Swift并没有像OC、Java那样的规定:任何类最终都要继承自某个基类
+ 子类可以重写父类的下标、方法、属性，重写必须加上override关键字
+ */
+
+//// 内存结构
+//class Animal {
+//   var age = 0
+//}
+//class Dog : Animal {
+//   var weight = 0
+//}
+//class ErHa : Dog {
+//   var iq = 0
+//}
+
+// MARK: 重写实例方法、下标
+//class Animal {
+//    func speak() {
+//        print("Animal speak")
+//    }
+//    subscript(index: Int) -> Int {
+//        return index
+//    }
+//}
+//class Cat : Animal {
+//    override func speak() {
+//        super.speak()
+//        print("Cat speak")
+//    }
+//    override subscript(index: Int) -> Int {
+//        return super[index] + 1
+//    }
+//}
+//var anim: Animal
+//anim = Animal()
+//// Animal speak
+//anim.speak()
+//// 6
+//print(anim[6])
+//anim = Cat()
+//// Animal speak
+//// Cat speak
+//anim.speak()
+//// 7
+//print(anim[6])
+
+/*
+ 被class修饰的类型方法、下标，允许被子类重写
+ 被static修饰的类型方法、下标，不允许被子类重写
+ */
+//class Animal {
+//    class func speak() {
+//        print("Animal speak")
+//    }
+//    class subscript(index: Int) -> Int {
+//        return index
+//    }
+//}
+//// Animal speak
+//Animal.speak()
+//// 6
+//print(Animal[6])
+//class Cat : Animal {
+//    override class func speak() {
+//        super.speak()
+//        print("Cat speak")
+//    }
+//    override class subscript(index: Int) -> Int {
+//        return super[index] + 1
+//    }
+//
+//}
+//// Animal speak
+//// Cat speak
+//Cat.speak()
+//// 7
+//print(Cat[6])
+
+// MARK:- 重写属性
+/*
+ 重写属性
+ 子类可以将父类的属性(存储、计算)重写为计算属性
+ 子类不可以将父类属性重写为存储属性
+ 
+ 只能重写var属性，不能重写let属性
+ 
+ 重写时，属性名、类型要一致
+ 
+ 子类重写后的属性权限 不能小于 父类属性的权限
+ 如果父类属性是只读的，那么子类重写后的属性可以是只读的、也可以是可读写的
+ 如果父类属性是可读写的，那么子类重写后的属性也必须是可读写的
+ */
+
+// MARK: 重写实例属性
+//class Circle {
+//    var radius: Int = 0
+//    var diameter: Int {
+//        set {
+//            print("Circle setDiameter")
+//            radius = newValue / 2
+//        }
+//        get {
+//            print("Circle getDiameter")
+//            return radius * 2
+//        }
+//    }
+//}
+//var circle: Circle
+//circle = Circle()
+//circle.radius = 6
+//// Circle getDiameter
+//// 12
+//print(circle.diameter)
+//// Circle setDiameter
+//circle.diameter = 20
+//// 10
+//print(circle.radius)
+
+//class SubCircle : Circle {
+//    override var radius: Int {
+//        set {
+//            print("SubCircle setRadius")
+//            super.radius = newValue > 0 ? newValue : 0
+//        }
+//        get {
+//            print("SubCircle getRadius")
+//            return super.radius
+//        }
+//    }
+//    override var diameter: Int {
+//        set {
+//            print("SubCircle setDiameter") super.diameter = newValue > 0 ? newValue : 0
+//        }
+//        get {
+//            print("SubCircle getDiameter")
+//            return super.diameter
+//        }
+//    }
+//
+//}
+//circle = SubCircle()
+//// SubCircle setRadius
+//circle.radius = 6
+//// SubCircle getDiameter // Circle getDiameter // SubCircle getRadius // 12 print(circle.diameter)
+//// SubCircle setDiameter
+//// Circle setDiameter
+//// SubCircle setRadius
+//circle.diameter = 20
+//// SubCircle getRadius
+//// 10
+//print(circle.radius)
+
+// MARK: 重写类型属性
+
+/*
+ 重写类型属性
+ 
+ 被class修饰的计算类型属性，可以被子类重写
+ 
+ 被static修饰的类型属性(存储、计算)，不可以被子类重写
+ */
+//class Circle {
+//    static var radius: Int = 0
+//    class var diameter: Int {
+//        set {
+//            print("Circle setDiameter")
+//            radius = newValue / 2
+//        }
+//        get {
+//            print("Circle getDiameter")
+//            return radius * 2
+//        }
+//    }
+//}
+//class SubCircle : Circle {
+//    override static var diameter: Int {
+//        set {
+//            print("SubCircle setDiameter")
+//            super.diameter = newValue > 0 ? newValue : 0
+//        }
+//        get {
+//            print("SubCircle getDiameter")
+//            return super.diameter
+//        }
+//    }
+//}
+//Circle.radius = 6
+//// Circle getDiameter
+//// 12
+//print(Circle.diameter)
+//// Circle setDiameter
+//Circle.diameter = 20
+//// 10
+//print(Circle.radius)
+//SubCircle.radius = 6
+//// SubCircle getDiameter
+//// Circle getDiameter
+//// 12
+//print(SubCircle.diameter)
+//// SubCircle setDiameter
+//// Circle setDiameter
+//SubCircle.diameter = 20
+//// 10
+//print(SubCircle.radius)
+
+// MARK: 属性观察器
+
+/*
+ 属性观察器
+ 
+ 可以在子类中为父类属性(除了只读计算属性、let属性)增加属性观察器
+ 
+ */
+
+// 重写 存储属性 观察器
+//class Circle {
+//    var radius: Int = 1
+//}
+//class SubCircle : Circle {
+//    override var radius: Int {
+//        willSet {
+//            print("SubCircle willSetRadius", newValue) }
+//        didSet {
+//            print("SubCircle didSetRadius", oldValue, radius)
+//        }
+//
+//    }
+//}
+//var circle = SubCircle()
+//// SubCircle willSetRadius 10
+//// SubCircle didSetRadius 1 10
+//circle.radius = 10
+
+// 重写 带观察器的存储属性 观察器
+//class Circle {
+//    var radius: Int = 1 {
+//        willSet {
+//            print("Circle willSetRadius", newValue)
+//        }
+//        didSet {
+//            print("Circle didSetRadius", oldValue, radius) }
+//    }
+//
+//}
+//class SubCircle : Circle {
+//    override var radius: Int {
+//        willSet {
+//            print("SubCircle willSetRadius", newValue)
+//        }
+//        didSet {
+//            print("SubCircle didSetRadius", oldValue, radius) }
+//    }
+//}
+//var circle = SubCircle()
+//// SubCircle willSetRadius 10
+//// Circle willSetRadius 10
+//// Circle didSetRadius 1 10
+//// SubCircle didSetRadius 1 10
+//circle.radius = 10
+
+// 重写 计算属性 属性观察器
+//class Circle {
+//    var radius: Int {
+//        set {
+//            print("Circle setRadius", newValue)
+//        }
+//        get {
+//            print("Circle getRadius")
+//            return 20
+//        }
+//    }
+//
+//}
+//class SubCircle : Circle {
+//    override var radius: Int {
+//        willSet {
+//            print("SubCircle willSetRadius", newValue)
+//        }
+//        didSet {
+//            print("SubCircle didSetRadius", oldValue, radius)
+//
+//        }
+//    }
+//}
+//var circle = SubCircle()
+//// Circle getRadius
+//// SubCircle willSetRadius 10
+//// Circle setRadius 10
+//// Circle getRadius
+//// SubCircle didSetRadius 20 20
+//circle.radius = 10
+
+
+
+//// 重写 类型属性 属性观察器
+//class Circle {
+//    class var radius: Int {
+//        set {
+//            print("Circle setRadius", newValue)
+//        }
+//        get {
+//            print("Circle getRadius")
+//            return 20
+//
+//        }
+//    }
+//}
+//class SubCircle : Circle {
+//    override static var radius: Int {
+//        willSet {
+//            print("SubCircle willSetRadius", newValue)
+//        }
+//        didSet {
+//            print("SubCircle didSetRadius", oldValue, radius)
+//
+//        }
+//    }
+//}
+//// Circle getRadius
+//// SubCircle willSetRadius 10
+//// Circle setRadius 10
+//// Circle getRadius
+//// SubCircle didSetRadius 20 20
+//SubCircle.radius = 10
+
+// MARK: final
+/*
+ 被final修饰的方法、下标、属性，禁止被重写
+ 被final修饰的类，禁止被继承
+ */
+ 
